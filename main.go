6d71dc79eae089e8768147f0a17f6270fa88b756
William Porto <powilliam19@gmail.com>
2020-06-18 01:05:57 -0400

    feat: write and read a file with encoded proto message

--- main.go
+++ main.go
@@ -2,9 +2,15 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
+
+	"google.golang.org/protobuf/proto"
 )
 
 func main() {
+	filename := "persons"
+	
 	p := &Person{
 		Id: 1,
 		Name: "William Porto",
@@ -14,5 +20,23 @@ func main() {
 		},
 	}
 
-	fmt.Println(p)
+	out, err := proto.Marshal(p)
+	if err != nil {
+		log.Fatalln("> Failed to encode: ", err)
+	}
+
+	if err := ioutil.WriteFile(filename, out, 0644); err != nil {
+		log.Fatalln("> Failed to write persons file: ", err)
+	}
+
+	in, err := ioutil.ReadFile(filename)
+	if err != nil {
+		log.Fatalln("> Error reading file:", err)
+	}
+	persons := &Person{}
+	if err := proto.Unmarshal(in, persons); err != nil {
+		log.Fatalln("> Failed to parse address book:", err)
+	}
+
+	fmt.Println(persons)
 }
